422754521124bca1e6d8945b51ea5f3b3ce4fe72
cjnj <151698129+cjnj@users.noreply.github.com>
2024-01-23 20:02:27 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 //global variables
 let iconImportant = false;
 function saveTask() {
-console.log("Saving task");
+  console.log("Saving task");
 }
 
 function changeIcon() {
-console.log("Changing icon");
-const nonImportant = "fa-solid fa-umbrella";
-const isImportant = "fa-solid fa-mug-hot";
-if (iconImportant) {
+  console.log("Changing icon");
+  const nonImportant = "fa-solid fa-umbrella";
+  const isImportant = "fa-solid fa-mug-hot";
+  if (iconImportant) {
     $("#iImportant").removeClass(isImportant).addClass(nonImportant);
     iconImportant = false;
-} else {
+  } else {
     $("#iImportant").removeClass(nonImportant).addClass(isImportant);
     iconImportant = true;
-}
+  }
   //please try to restore the icon
 }
 
 function init() {
-console.log("This is the parent of everything");
+  console.log("This is the parent of everything");
   //load data
 
   //hook events
-$("#btnSave").click(saveTask);
-$("#iImportant").click(changeIcon);
+  $("#btnSave").click(saveTask);
+  $("#iImportant").click(changeIcon);
   //document.getElementById("btnSave").click(saveTask);
 }
 
